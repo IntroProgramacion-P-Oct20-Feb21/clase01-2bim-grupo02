15944dab0f19bc8080211db0357d0dfba37bcaa0
unknown <dacuenca7@utpl.edu.ec>
2020-12-09 11:47:13 -0500

    Clase 1

--- Ejemplos/src/ejemplos/Ejercicio01.java
+++ Ejemplos/src/ejemplos/Ejercicio01.java
@@ -10,11 +10,29 @@ package ejemplos;
  * @author USUARIO
  */
 public class Ejercicio01 {
-     public static void main(String[] args) {
- 
+
+    public static void main(String[] args) {
+        String frase = "Hola Mundo";
+        char posicion;
         int i;
-        for(i=1;i< 5;i++){
-            System.out.println(i);
+        int conMayus = 0;
+        int conMin = 0;
+        String cadena = "Frase con:\n";
+        for (i = 0; i < frase.length(); i++) { //Minusculas
+            posicion = frase.charAt(i);
+            if ((((int) posicion) >= 65) && (((int) posicion) <= 90)) {
+                conMayus += 1;
+            } else {
+                if ((((int) posicion) >= 97) && (((int) posicion) <= 122)) {
+                    conMin += 1;
+
+                }
+
+            }
+            
         }
-    }
+    System.out.printf("%sNúmero de minusculas: %d\nNúmero de Mayusculas: %d\n",
+                    cadena, conMin, conMayus);
+
+}
 }
