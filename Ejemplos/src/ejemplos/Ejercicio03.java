608408afdb809cf9e50c564d87c20945b592efaf
Michael Pereira <michaelmaldonadopereira69@gmail.com>
2020-12-09 12:11:18 -0500

    Nuevo ejercicio03

---
+++ Ejemplos/src/ejemplos/Ejercicio03.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio03 {
+    public static void main(String[]args){
+	int contador;
+	String punto = ("*"),cadenaAsteriscos2 = ("*");
+	System.out.println(punto);
+	for (contador = 1;(!(contador == 10)) ; contador++) {
+            punto = punto + cadenaAsteriscos2;
+            System.out.println(punto);
+	}
+    }
+}
+
